d0824fe23ded7a0198d5bdf210b3fc94e1d45613
Starlord0alpha <notgoatmichael@gmail.com>
2022-07-29 17:02:57 +0800

    Add associated comments to a blog post

--- vue/src/store/generated/blog/blog.blog/module/index.ts
+++ vue/src/store/generated/blog/blog.blog/module/index.ts
@@ -4,11 +4,15 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgCreateComment } from "./types/blog/tx";
 import { MsgCreatePost } from "./types/blog/tx";
+import { MsgDeleteComment } from "./types/blog/tx";
 
 
 const types = [
+  ["/blog.blog.MsgCreateComment", MsgCreateComment],
   ["/blog.blog.MsgCreatePost", MsgCreatePost],
+  ["/blog.blog.MsgDeleteComment", MsgDeleteComment],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -41,7 +45,9 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgCreateComment: (data: MsgCreateComment): EncodeObject => ({ typeUrl: "/blog.blog.MsgCreateComment", value: MsgCreateComment.fromPartial( data ) }),
     msgCreatePost: (data: MsgCreatePost): EncodeObject => ({ typeUrl: "/blog.blog.MsgCreatePost", value: MsgCreatePost.fromPartial( data ) }),
+    msgDeleteComment: (data: MsgDeleteComment): EncodeObject => ({ typeUrl: "/blog.blog.MsgDeleteComment", value: MsgDeleteComment.fromPartial( data ) }),
     
   };
 };
--- x/blog/keeper/msg_server_create_post.go
+++ x/blog/keeper/msg_server_create_post.go
@@ -12,9 +12,11 @@ func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (
 
 	// Create variable of type Post
 	var post = types.Post{
-		Creator: msg.Creator,
-		Title:   msg.Title,
-		Body:    msg.Body,
+		Creator:   msg.Creator,
+		Id:        msg.Id,
+		Title:     msg.Title,
+		Body:      msg.Body,
+		CreatedAt: ctx.BlockHeight(),
 	}
 
 	// Add a post to the store and get back the ID
